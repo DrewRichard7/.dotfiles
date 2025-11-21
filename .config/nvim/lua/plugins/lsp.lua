a2f644bdca2b18060373ae2afc4e0b09ce37b42c
Andrew Richard <43729071+DrewRichard7@users.noreply.github.com>
2025-11-20 18:01:18 -0800

    Automated archiving commit: .config/ghostty/config,.config/nvim/lazy-lock.json,.config/nvim/lua/plugins/colorizer.lua,.config/nvim/lua/plugins/lsp.lua,.config/nvim/lua/plugins/theme.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -63,39 +63,3 @@ return {
     },
   },
 }
-
--- below is a working lsp; above is trying to restore neovim's default global lsp keymaps
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         lua_ls = {},
---         -- basedpyright = {
---         --   settings = {
---         --     basedpyright = {
---         --       analysis = {
---         --         typeCheckingMode = "standard",
---         --       },
---         --     },
---         --   },
---         -- },
---         pyright = {},
---         ruff = {},
---         jsonls = {},
---         yamlls = {},
---         rust_analyzer = {},
---       },
---     },
---     -- capabilities = {},
---   },
---   {
---     "mason-org/mason.nvim",
---     opts = {
---       ensure_installed = {
---         "stylua", -- formatter only
---         -- other tools that aren't LSP servers
---       },
---     },
---   },
--- }
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -15,7 +15,7 @@ return {
   -- lua/plugins/rose-pine.lua
   {
     "rose-pine/neovim",
-    enabled = false,
+    enabled = true,
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
@@ -29,6 +29,7 @@ return {
   {
     name = "black-diamond",
     dir = "/Users/andrew/dev/nvim-plugins/black-diamond.nvim/",
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
@@ -36,6 +37,94 @@ return {
         transparent = true,
       })
       vim.cmd.colorscheme("black-diamond")
+    end,
+  },
+  {
+    "ribru17/bamboo.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        style = "multiplex",
+        lualine = {
+          transparent = true,
+        },
+      })
+      require("bamboo").load()
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = true },
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        transparent = true,
+        dimInactive = false,
+        terminalColors = true,
+        theme = "wave",
+        background = { dark = "wave", light = "lotus" },
+        colors = {
+          -- palette = {},
+          theme = { all = { ui = { bg_gutter = "none" } } },
+        },
+        overrides = function(colors)
+          return { Visual = { bg = "#87ceeb" } }
+        end,
+      })
+      vim.cmd.colorscheme("kanagawa")
+      vim.api.nvim_set_hl(0, "TermCursor", { fg = "#A6E3A1", bg = "#A6E3A1" })
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    config = function()
+      vim.cmd.colorscheme("kanagawa")
+      vim.cmd([[
+  highlight! link SignColumn Normal
+  highlight! link LineNr     Normal
+  highlight! link FoldColumn Normal
+]])
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    enabled = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({})
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
+
+  {
+    "tiagovla/tokyodark.nvim",
+    enabled = false,
+    priority = 1000,
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      vim.cmd([[colorscheme tokyodark]])
+    end,
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme("kanagawa-paper-ink")
+      -- vim.cmd.colorscheme("kanagawa-paper")
     end,
   },
 }
